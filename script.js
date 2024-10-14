e9313133b686c67a9e4ed9a5489ee2d84f639f6e
gastongomezcuello <gastongomezcuello@gmail.com>
2024-10-14 05:16:06 +0000

    simulador@1.1.0 - se agrega carga de productos / lista por defecto / validacion de numeros / orden de lista de productos / cambio de inicio

--- script.js
+++ script.js
@@ -1,26 +1,86 @@
-// Declaración de variables y constantes
-const products = ["tijeras", "mochilas", "cajas", "cañas"];
+//Variables
+let products = [];
+let joinedProducts;
 let sales = [];
+let productsAmount;
+let defaultProducts = ["gorras", "sombreros", "camperas", "remeras"];
 
-//Agregar un método para hacer mayúscula la primer letra de un string
+// Función para cargar productos
+function chargeProducts(amount) {
+  for (let i = 0; i < amount; i++) {
+    let status;
+    while (status != "ok") {
+      let product;
+      product = prompt(`Ingresá tu producto número ${i + 1} :`);
+      if (product === null) {
+        alert(
+          "Proceso cancelado. Tu lista de productos será una cargada por defecto"
+        );
+        console.log(
+          "Proceso cancelado. Tu lista de productos será una cargada por defecto"
+        );
+        return (products = defaultProducts);
+      } else if (!isNaN(product) || product === "") {
+        alert(
+          "No se pudo cargar tu producto. Asegurate de no ingresar números ni dejar espacios en blanco"
+        );
+        console.log("Error al cargar nuevo producto");
+      } else {
+        products.push(product);
+        status = "ok";
+      }
+    }
+  }
+}
+
+// Agregar un método para hacer mayúscula la primer letra de un string
 String.prototype.firstCharToUpperCase = function () {
   return this.charAt(0).toUpperCase() + this.slice(1);
 };
+
+// Función para agrupar productos en una lista
+function joinProducts(products) {
+  joinedProducts = products
+    .map((product) => product.firstCharToUpperCase())
+    .join("\n");
+}
+
+// Función para validar e ingresar números positivos
+function validPositiveNumber(promptMessage) {
+  let amount = 0;
+  while (amount < 1 || isNaN(amount)) {
+    amount = parseInt(
+      prompt(
+        `${promptMessage}\n(para correcto llamado a funciones no podrás cancelar este proceso, disculpá las molestias)`
+      )
+    );
+    if (amount > 0) {
+      console.log("Cantidad ingresada correctamente");
+      return amount;
+    } else if (isNaN(amount)) {
+      console.log(
+        "El valor ingresado no es un número. Por favor ingresá solamente números"
+      );
+      alert(
+        "El valor ingresado no es un número. Por favor ingresá solamente números"
+      );
+    } else {
+      console.log("Debés ingresar un número positivo");
+      alert("Debés ingresar un número positivo");
+    }
+  }
+}
 
 // Función para registrar una venta
 function salesRegister() {
   let product;
   while (!products.includes(product)) {
     product = prompt(
-      `Ingrese el nombre del producto vendido:
-      \n${products[0].firstCharToUpperCase()} 
-      \n${products[1].firstCharToUpperCase()} 
-      \n${products[2].firstCharToUpperCase()} 
-      \n${products[3].firstCharToUpperCase()}`
+      `Escribí el producto que hayas vendido. Estos son los productos disponibles:\n${joinedProducts}`
     ).toLowerCase();
     if (product === null) {
       console.log(
-        "Proceso de registro cancelado. Actualice para volver a iniciar"
+        "Proceso de registro cancelado. Actualizá para volver a iniciar"
       );
       return;
     }
@@ -33,50 +93,38 @@ function salesRegister() {
       alert("El producto seleccionado no se encuentra entre las opciones");
     }
   }
-  let amount = 0;
-  while (amount < 1 || isNaN(amount)) {
-    amount = parseInt(prompt("Ingrese la cantidad vendida:"));
-    if (amount > 0) {
-      console.log("Cantidad ingresada correctamente");
-    } else if (isNaN(amount)) {
-      console.log("El valor ingresado no es un numero");
-      alert("El valor ingresado no es un numero");
-    } else {
-      console.log("Debes ingresar un número positivo");
-      alert("Debes ingresar un número positivo");
-    }
-  }
+  let amount = validPositiveNumber("Ingresá la cantidad vendida");
   sales.push({ product, amount });
   console.log(`Venta registrada: ${amount} unidades de ${product}`);
   alert(
-    `Venta registrada: ${amount} unidades de ${product} \n Continúa con el registro`
+    `Venta registrada: ${amount} unidades de ${product} \n Continuá con el registro`
   );
 }
 
 // Función para preguntar al usuario si quiere registrar un producto
 function askForRegister() {
-  let start;
+  let startRegister;
   do {
-    start = prompt("¿Quiere registrar una venta? (SI/NO)", "SI");
-    if (start === null) {
+    startRegister = prompt("¿Quiere registrar una venta? (SI/NO)", "SI");
+    if (startRegister === null) {
       console.log(
-        "Proceso de registro cancelado. Actualice para volver a iniciar"
+        "Proceso de registro cancelado. Actualizá para volver a iniciar"
       );
-      alert("Proceso de registro cancelado. Actualice para volver a iniciar");
+      alert("Proceso de registro cancelado. Actualizá para volver a iniciar");
       return;
     }
-    start = start.toUpperCase();
+    startRegister = startRegister.toUpperCase();
 
-    if (start === "SI") {
+    if (startRegister === "SI") {
       salesRegister();
-    } else if (start === "NO") {
+    } else if (startRegister === "NO") {
       console.log("Terminaste el proceso de registro");
       alert("Terminaste el proceso de registro");
     } else {
-      console.log("Ingrese una opción válida");
-      alert("Ingrese una opción válida");
+      console.log("Ingresá una opción válida");
+      alert("Ingresá una opción válida");
     }
-  } while (start !== "NO");
+  } while (startRegister !== "NO");
 }
 
 // Función para mostrar todas las ventas
@@ -92,11 +140,19 @@ function showSales() {
     console.log(`${sale.amount} unidades de ${sale.product}`);
   }
   alert(
-    `Registraste ${sales.length} ventas \n Puedes actualizar la pagina para volver a empezar`
+    `Registraste ${sales.length} ventas.\nPuedes actualizar la pagina para volver a empezar`
   );
 }
+//Saludo al usuario y petición de cantidad de productos
+alert(
+  "Bienvenido al simulador de registro!\nAquí podrás cargar tus productos y registrar tus ventas de manera fácil y rápida."
+);
+productsAmount = validPositiveNumber(
+  "Comencemos!\nIngresá el número de productos que quieres cargar a tu lista"
+);
 
-// Llamadas a las funciones (recordar que la funcion de registro se llama dentro de la funcion askForRegister)
-
+// Llamadas a las funciones (recordar que la función salesRegister() se llama dentro de la función askForRegister())
+chargeProducts(productsAmount);
+joinProducts(products);
 askForRegister();
 showSales();
