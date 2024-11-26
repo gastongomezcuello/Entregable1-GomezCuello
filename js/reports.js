0deb9e70d51e7f3b896803b28698360f2a8d7cc3
gastongomezcuello <gastongomezcuello@gmail.com>
2024-11-26 17:31:40 -0300

    listo-para-entrega-(???)

--- js/reports.js
+++ js/reports.js
@@ -59,70 +59,72 @@ function salesChart(chartID, productQties) {
   let data = Object.values(productQties);
 
   if (charts[chartID]) {
-    charts[chartID].destroy();
-  }
-  charts[chartID] = new Chart(ctx, {
-    type: "pie",
-    data: {
-      labels: labels,
-      datasets: [
-        {
-          label: "Unidades vendidas",
-          data: data,
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(100, 149, 237, 0.2)",
-            "rgba(240, 128, 128, 0.2)",
-            "rgba(154, 205, 50, 0.2)",
-            "rgba(255, 20, 147, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-            "rgba(100, 149, 237, 1)",
-            "rgba(240, 128, 128, 1)",
-            "rgba(154, 205, 50, 1)",
-            "rgba(255, 20, 147, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        legend: {
-          display: true,
-          labels: {
-            boxWidth: 20,
-            padding: 15,
-            textAlign: "left",
-            font: {
+    charts[chartID].data.labels = labels;
+    charts[chartID].data.datasets[0].data = data;
+    charts[chartID].update();
+  } else {
+    charts[chartID] = new Chart(ctx, {
+      type: "pie",
+      data: {
+        labels: labels,
+        datasets: [
+          {
+            label: "Unidades vendidas",
+            data: data,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+              "rgba(100, 149, 237, 0.2)",
+              "rgba(240, 128, 128, 0.2)",
+              "rgba(154, 205, 50, 0.2)",
+              "rgba(255, 20, 147, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+              "rgba(100, 149, 237, 1)",
+              "rgba(240, 128, 128, 1)",
+              "rgba(154, 205, 50, 1)",
+              "rgba(255, 20, 147, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              boxWidth: 20,
+              padding: 15,
+              textAlign: "left",
+              font: {
+                size: 11,
+              },
+            },
+            position: "bottom",
+          },
+          tooltip: {
+            enabled: true,
+            bodyFont: {
               size: 11,
             },
           },
-          position: "bottom",
-        },
-        tooltip: {
-          enabled: true,
-          bodyFont: {
-            size: 11,
-          },
         },
       },
-    },
-  });
-
+    });
+  }
   return canvas;
 }
 
